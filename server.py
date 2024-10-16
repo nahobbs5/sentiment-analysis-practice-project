40669a6eb2d81d97ee9d14cd197013c1d9396517
nahobbs5 <nahobbs@gmail.com>
2024-10-16 12:15:01 -0400

    created sentiment_analysis.py

--- server.py
+++ server.py
@@ -4,8 +4,11 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
+app = Flask(__name__)
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
